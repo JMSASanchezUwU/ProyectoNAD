902351f1f11d25864dd05adfc8b5f57c96c0b311
estrellasoto <estrellajuarez8soto@gmail.com>
2023-03-15 11:02:06 -0600

    Proyecto para materia base de datos para computo en la nube

---
+++ servidor/routes/usuario.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const router = express.Router();
+const usuarioController = require('../controllers/usuarioController');
+
+//api rutas
+router.post('/',usuarioController.crearUsuario);
+router.get('/', usuarioController.obtenerUsuarios);
+router.put('/:id', usuarioController.actualizarUsuario);
+router.get('/:id', usuarioController.obtenerUsuario);
+router.delete('/:id', usuarioController.eliminarUsuarioLog);
+
+
+module.exports = router;
